6628e05ed7202913a05b7f8e511b0c20ef3d060e
Sera <54048830+Dangomimikyu@users.noreply.github.com>
2026-03-08 23:18:32 +0800

    modified praw_test
    put the data into a df, i think we probably need to use a ipynb later on so we can see the df better

--- src/testing/praw_test.py
+++ src/testing/praw_test.py
@@ -1,4 +1,5 @@
 import praw as pp
+import pandas as pd
 
 # im using the 'test' app in the reddit account
 # can find the apps by going to reddit.com/prefs/apps 
@@ -9,12 +10,6 @@ reddit = pp.Reddit( client_id = "s0Epgs8jsHkNKzsFGCnw-w",
                     client_secret = None,
                     redirect_uri="http://localhost:8080",
                     user_agent = "python:school_project:v1.0.0 (by u/4021-30)")
-
-# test that it's working, get the 10 latest posts and their comments in r/soccer
-for submission in reddit.subreddit("soccer").new(limit=10):
-    top_level_comments = list(submission.comments)
-    all_comments = submission.comments.list()
-    print(submission.title)
 
 subreddits = [
     # main subreddits
@@ -43,3 +38,31 @@ subreddits = [
     "ACMilan",
 ]
 
+# test that it's working, get the 10 latest posts and their comments in r/soccer
+for submission in reddit.subreddit("soccer").new(limit=10):
+    top_level_comments = list(submission.comments)
+    all_comments = submission.comments.list()
+    print("[\nsubmission info:\n" +
+          " |upvotes     : " + str(submission.score) + "\n" + 
+          " |upvote ratio: " + str(submission.upvote_ratio) + "\n" +
+          " |author      : " + str(submission.author.name) + "\n" +
+          " |author flair: " + str(submission.author_flair_text) + "\n" +
+          " |title       : " + submission.title + "\n]")
+    
+# send the collected data into a csv, can use for classification in the classification.ipynb file
+dfRows = []
+for submission in reddit.subreddit("soccer").new(limit=10):
+    tempRow = {
+        "author": str(submission.author),
+        "author_flair": submission.author_flair_text,
+        "title": submission.title,
+        "post_id": submission.id,
+        "upvotes": submission.score,
+        "upvote_ratio": submission.upvote_ratio,
+        "num_comments": submission.num_comments,
+        "subreddit" : submission.subreddit.name
+    }
+    dfRows.append(tempRow)
+
+df = pd.DataFrame(dfRows)
+print(df)
